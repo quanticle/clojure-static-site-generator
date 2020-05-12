f3413aab46baa149c81850374c6aab5f7d593d37
Rohit Patnaik <rohit.patnaik@optum.com>
2020-05-12 11:05:21 -0500

    2020-05-12: Initial commit

---
+++ test/static_site_generator/core_test.clj
@@ -0,0 +1,7 @@
+(ns static-site-generator.core-test
+  (:require [clojure.test :refer :all]
+            [static-site-generator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
